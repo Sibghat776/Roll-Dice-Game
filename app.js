41e37e048bf8a167d02dda6eaa5483facdf7edad
Sibghat776 <ullahsibghat786@gmail.com>
2024-11-04 20:27:16 +0500

    Logic building started

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 let user1 = prompt("Enter your Name , Mr Player 1")
 let user2 = prompt("Enter your Name , Mr Player 2")
+
 let userName1 = document.querySelector(".name1")
 let userName2 = document.querySelector(".name2")
+
+let randomNumber = Math.ceil(Math.random() *6);
+let image = document.querySelector('#img')
+let count = 0
+
+
 userName1.innerHTML = user1
 userName2.innerHTML = user2
+
+function rollDice() {
+    image.src = `./images/${randomNumber}.png`
+    console.log(randomNumber);
+    
+    
+}
