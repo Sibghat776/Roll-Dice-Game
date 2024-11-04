3aba6a7547cdc4dcae44811ad98cacaa7d98da2a
Sibghat776 <ullahsibghat786@gmail.com>
2024-11-04 12:14:03 +0500

    UI design on Progress

---
+++ app.js
@@ -0,0 +1,6 @@
+let user1 = prompt("Enter your Name , Mr Player 1")
+let user2 = prompt("Enter your Name , Mr Player 2")
+let userName1 = document.querySelector(".name1")
+let userName2 = document.querySelector(".name2")
+userName1.innerHTML = user1
+userName2.innerHTML = user2
