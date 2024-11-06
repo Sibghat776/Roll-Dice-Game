8ffd34950cb12ae03a4538b4aa8dddbd0b2cf90a
Sibghat776 <ullahsibghat786@gmail.com>
2024-11-06 20:57:23 +0500

    Complete

--- app.js
+++ app.js
@@ -1,42 +1,64 @@
-let user1 = prompt("Enter your Name , Mr Player 1")
-let user2 = prompt("Enter your Name , Mr Player 2")
+const player1 = prompt("Write your name, Mr Player 1");
+const player2 = prompt("Write your name, Mr Player 2");
 
-document.querySelector(".name1").innerHTML = user1
-document.querySelector(".name2").innerHTML = user2
+document.querySelector(".name1").innerHTML = player1;
+document.querySelector(".name2").innerHTML = player2;
 
-let randomNumber = Math.ceil(Math.random() * 6);
-let image = document.querySelector('#img')
+const player1Element = document.querySelector(".player-1");
+const player2Element = document.querySelector(".player-2");
 
-let currentScore1 = document.querySelector("#currentScore1")
-let currentScore2 = document.querySelector("#currentScore2")
+let currentPlayer = true;
 
-let player1 = document.querySelector(".player-1")
-let player2 = document.querySelector(".player-2")
-
-player1.classList.add("activePlayer")
-player2.classList.remove("activePlayer")
-
-let player;
-
+let currentScore1 = 0;
+let currentScore2 = 0;
 
 function rollDice() {
-    let randomNumber = Math.ceil(Math.random() * 6);
-    image.src = `./images/${randomNumber}.png`
-    if (randomNumber != 1) {
-        if (player = true) {
-            currentScore1.innerHTML = randomNumber + +currentScore1.innerHTML
-        } else {
-            currentScore2.innerHTML = randomNumber + +currentScore2.innerHTML
-        }
+  console.log("==>> chala");
+  const diceNumber = Math.ceil(Math.random() * 6);
+  console.log(diceNumber, "==>> diceNumber");
+  console.log(document.querySelector("img").src);
+  document.querySelector("img").src = `./Images/${diceNumber}.png`;
+  if (diceNumber !== 1) {
+    if (currentPlayer) {
+      currentScore1 += diceNumber;
+      document.querySelector("#currentScore1").innerHTML = currentScore1;
     } else {
-        if (player) {
-            currentScore1.innerHTML = 0
-        } else {
-            currentScore2.innerHTML = 0
-        }
-
-        player !== player
-        player1.classList.toggle("activePlayer")
-        player2.classList.toggle("activePlayer")
+      currentScore2 += diceNumber;
+      document.querySelector("#currentScore2").innerHTML = currentScore2;
     }
+  } else {
+    if (currentPlayer) {
+      currentScore1 = 0;
+      document.querySelector("#currentScore1").innerHTML = currentScore1;
+    } else {
+      currentScore2 = 0;
+      document.querySelector("#currentScore2").innerHTML = currentScore2;
+    }
+    currentPlayer = !currentPlayer;
+  }
+}
+
+function hold() {
+  if (currentPlayer) {
+    let score1Html = document.querySelector(".score1");
+    score1Html.innerHTML = +score1Html.innerHTML + currentScore1;
+    currentScore1 = 0;
+    document.querySelector("#currentScore1").innerHTML = currentScore1;
+    if (score1Html.innerHTML >= 100) {
+      document.querySelector(
+        ".main"
+      ).innerHTML = `<h1>Winner is ${player1}</h1>`;
+    }
+  } else {
+    let score2Html = document.querySelector(".score2");
+    score2Html.innerHTML = +score2Html.innerHTML + currentScore2;
+    currentScore2 = 0;
+    document.querySelector("#currentScore2").innerHTML = currentScore2;
+    if (score2Html.innerHTML >= 100) {
+      document.querySelector(
+        ".main"
+      ).innerHTML = `<h1>Winner is ${player2}</h1>`;
+    }
+  }
+  currentPlayer = !currentPlayer;
 }
